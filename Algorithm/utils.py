b3067a8c05719255e14f8af198d3f33ce439ff9e
maoyingxue <ymao46972@gmail.com>
2019-07-10 15:36:45 +0800

    Update utils.py

--- Algorithm/utils.py
+++ Algorithm/utils.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jul  5 09:49:54 2019
-
-@author: maoyingxue
 """
-
+#类型和颜色对应信息
+type2color={1:["green","red"],
+            2:["black","red"],
+            3:["green","blue"],
+            4:["green","red"],
+            5:["grey","blue"],
+            6:["blue","red"] 
+            }
