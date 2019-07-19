9e9128c9413fca5e6a847aad7bc4ad65056cd191
XiangDe <qq313700046@icloud.com>
2019-07-19 22:03:22 +0800

    call grid analysis function to Interface.py and add a test script for Grid Analyzer

--- Interface.py
+++ Interface.py
@@ -2,23 +2,31 @@
 """
 Created on Thu Jun 20 11:35:32 2019
 
-@author: maoyingxue
+@author: maoyingxuex
 """
 """
 info:为接口定义的json输入文件
 results：为定义的json输出文件
 """
-#计算元分类
+from Algorithm.grid_analysis import grid_analyzer
+
+
+# 计算元分类
 def calType(img):
-    #输入图片
+    # 输入图片
     return results
-#计算边界点
+
+
+# 计算边界点
 def calPoints(info):
     return results
-#计算行列信息
-def calGridnums(info):
-    return results
-#计算端口信息
+
+
+# 计算行列信息
+def calGridInfo(info):
+    return grid_analyzer.analysis(info)
+
+
+# 计算端口信息
 def predictPorts(info):
     return results
-    
--- TestSample.py
+++ TestSample.py
@@ -5,3 +5,37 @@ Created on Fri Jul  5 09:34:59 2019
 @author: maoyingxue
 """
 
+from Interface import *
+import cv2
+from constant import *
+from utils import correct, getpoints
+
+
+def testGridAnalyzer():
+    resize_dir = 'origin_data/resize'
+    if not os.path.exists(resize_dir):
+        os.mkdir(resize_dir)
+    resize_path = os.path.join(resize_dir, 'type4_3.jpg')
+    img_path = "origin_data/test/type4_3.jpg"
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
+    frame_type = 4
+    if img is None:
+        print("Img is None.")
+
+    points = getpoints.getPoints(img, frame_type)
+    assert len(points) == 4
+    img_corrected = correct.transform(img, points)
+    cv2.imwrite(resize_path, img_corrected)
+    info = {
+        OUTER_COLOR: 'green',
+        INNER_COLOR: 'red',
+        ADDR: resize_path,
+        POINTS: points
+    }
+    res = calGridInfo(info)
+    print(res)
+
+
+if __name__ == '__main__':
+    testGridAnalyzer()
